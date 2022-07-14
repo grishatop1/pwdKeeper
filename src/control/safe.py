dd736214849ce7213254827056b91a6376298189
grisha <grigorije05@gmail.com>
2022-07-14 14:37:48 +0300

    Update safe.py

--- src/control/safe.py
+++ src/control/safe.py
@@ -1,5 +1,7 @@
 import os
 import base64
+import pickle
+from io import BytesIO
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -12,6 +14,8 @@ class SafeControl:
         self.path: str = None
         self.f = None
         self.fnet: Fernet = None
+        self.data = []
+        self.prepend = BytesIO()
         
     def setPath(self, path):
         self.path = path
@@ -39,11 +43,28 @@ class SafeControl:
         _hash = kdf_hash.derive(key)
 
         self.fnet = Fernet(key)
-        self.f.write(salt)
-        self.f.write(_hash)
-        
+        self.prepend.write(salt)
+        self.prepend.write(_hash)
+        self.save()
         #salt16 + hash32 = first 48 bytes for auth
         
     def close(self, *args):
         if self.f:
             self.f.close()
+            
+    def save(self):
+        data = pickle.dumps(self.data)
+        ciphertext = self.fnet.encrypt(data)
+        prepend = self.prepend.read()
+        self.f.write(prepend)
+        self.f.write(ciphertext)
+        self.prepend.seek(0)
+            
+    def addAccount(self, service, username, password):
+        acc = {
+            "service": service,
+            "username": username,
+            "password": password
+        }
+        self.data.append(acc)
+        self.save()
