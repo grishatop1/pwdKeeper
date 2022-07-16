888f6dda18b4444f1cf06e7897b62b0d80668296
grisha <grigorije05@gmail.com>
2022-07-16 17:09:51 +0300

    check multithreaded

--- src/control/login.py
+++ src/control/login.py
@@ -1,3 +1,6 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtCore import QThread
+
 class LoginCtrl:
     def __init__(self, ctrl):
         self.ctrl = ctrl
@@ -9,15 +12,34 @@ class LoginCtrl:
         self.ctrl.ui.login_page.pwd_entry.textChanged.connect(self.hideError)
         
     def proceed(self):
-        pwd = self.ctrl.ui.login_page.pwd_entry.text()
-        result = self.ctrl.safe.load(pwd)
-        if not result:
-            self.showError("Wrong password!")
-            return
         
-        self.ctrl.cache.writePath()
-        self.ctrl.main.loadEverything()
-        self.ctrl.ui.stacked.setCurrentIndex(3)
+        self.ctrl.ui.login_page.enter_btn.setText("Checking...")
+        self.ctrl.ui.login_page.enter_btn.setDisabled(True)
+        
+        pwd = self.ctrl.ui.login_page.pwd_entry.text()
+        
+        self.thread = QThread()
+        self.worker = Worker(self.ctrl, pwd)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.done_signal.connect(self.thread.quit)
+        self.worker.done_signal.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+        
+        self.thread.finished.connect(
+            lambda: self.done(self.worker.result)
+        )
+        
+    def done(self, result):
+        if result:
+            self.ctrl.cache.writePath()
+            self.ctrl.main.loadEverything()
+            self.ctrl.ui.stacked.setCurrentIndex(3)
+        else:
+            self.showError("Wrong password!")
+            self.ctrl.ui.login_page.enter_btn.setText("Enter!")
+            self.ctrl.ui.login_page.enter_btn.setDisabled(False)
         
     def showError(self, text):
         self.ctrl.ui.login_page.error_label.setText(text)
@@ -29,3 +51,15 @@ class LoginCtrl:
     def resetForm(self):
         self.hideError()
         self.ctrl.ui.login_page.pwd_entry.setText("")
+        
+class Worker(QObject):
+    done_signal = pyqtSignal()
+    def __init__(self, ctrl, pwd):
+        super().__init__()
+        self.ctrl = ctrl
+        self.pwd = pwd
+        self.result = None
+        
+    def run(self):
+        self.result = self.ctrl.safe.load(self.pwd)
+        self.done_signal.emit()
